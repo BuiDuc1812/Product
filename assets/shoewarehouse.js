7e44ddff00b950e1761e6aa3bd9515ced16a4410
BuiDuc1812 <db939397@gmail.com>
2023-03-16 14:48:39 +0700

    add js

--- assets/shoewarehouse.js
+++ assets/shoewarehouse.js
@@ -1,6 +1,21 @@
 
 let productTabs = document.querySelector('.product-tabs');
+
 if (productTabs) {
-    let titletabs = productTabs.querySelectorAll('.tabs-title');
+    let titleTabs = productTabs.querySelectorAll('.tabs-title');
     let contentTabs = productTabs.querySelectorAll('.data-tabs');
+    titleTabs.forEach(item=>{
+        let attribute = item.getAttribute("data-title-name");
+        let content = productTabs.querySelector('[data-block-name]'.concat(attribute,']'));
+        item.addEventListener('click',function(){
+            titleTabs.forEach(item=>{
+                item.classList.remove('active');
+            })
+            contentTabs.forEach(item=>{
+                item.classList.add('hidden');
+            })
+            item.classList.add('active');
+            item.classList.remove('hidden');
+        })
+    })
 }
