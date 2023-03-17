a9d2d460177bf1624f512447d3d2757dd4afd5ee
BuiDuc1812 <db939397@gmail.com>
2023-03-17 17:33:16 +0700

    change

--- assets/shoewarehouse.js
+++ assets/shoewarehouse.js
@@ -1,24 +1,31 @@
-
-let productTabs = document.querySelector('.product-tabs');
-
-if (productTabs) {
-    let titleTabs = productTabs.querySelectorAll('.tabs-title');
-    let contentTabs = productTabs.querySelectorAll('.data-tabs');
-    titleTabs.forEach(item=>{
-        let attribute = item.getAttribute("data-title-name");
-        let content = productTabs.querySelector('[data-block-name='.concat(attribute,']'));
-        item.addEventListener('click',function(){
+class ProductJs extends HTMLElement {
+    constructor() {
+        super();
+        console.log(this);
+    }
+    productTabs(){
+        let productTabs = document.querySelector('.product-tabs');
+        if (productTabs) {
+            let titleTabs = productTabs.querySelectorAll('.tabs-title');
+            let contentTabs = productTabs.querySelectorAll('.data-tabs');
             titleTabs.forEach(item=>{
-                item.classList.remove('active');
+                let attribute = item.getAttribute("data-title-name");
+                let content = productTabs.querySelector('[data-block-name='.concat(attribute,']'));
+                item.addEventListener('click',function(){
+                    titleTabs.forEach(item=>{
+                        item.classList.remove('active');
+                    })
+                    contentTabs.forEach(item=>{
+                        item.classList.add('hidden');
+                    })
+                    item.classList.add('active');
+                    content.classList.remove('hidden');
+                })
             })
-            contentTabs.forEach(item=>{
-                item.classList.add('hidden');
-            })
-            item.classList.add('active');
-            content.classList.remove('hidden');
-        })
-    })
+        }
+    }
 }
+customElements.define('product', ProductJs);
 
 var init = false;
 var swiper = '';
