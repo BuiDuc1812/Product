bd07706fd331e105decdbf671aa3fd83bc1fb9d3
BuiDuc1812 <db939397@gmail.com>
2023-03-20 17:32:22 +0700

    style

--- assets/accordion.js
+++ assets/accordion.js
@@ -1,59 +1,27 @@
 let arrcodion = document.querySelectorAll('.footer-block--menu');
-if(accordion){
+if(arrcodion){
   accordionDrawer()
 }
 function accordionDrawer(){
-  accordion.forEach((item, index)=>{
+    arrcodion.forEach((item, index)=>{
     addOpen(item,index)
   })
 }
 
 function addOpen(params, ind){
   var header = params.querySelector('.footer-block__heading');
-  console.log(header)
-//   header.addEventListener('click',()=>{
-//     params.classList.toggle('open');
-//     let description = params.querySelector('.accordion-description');
-//     if(params.classList.contains('open')){
-//       params.querySelector('.down').classList.add('turn-off');
-//       params.querySelector('.up').classList.remove('turn-off');
-//       description.style.height = `${description.scrollHeight}px`;
-//     } else{
-//       description.style.height = '0';
-//       params.querySelector('.down').classList.remove('turn-off');
-//       params.querySelector('.up').classList.add('turn-off');
-//     }
-//     removeOpen(ind);
-//   })
+  header.addEventListener('click',()=>{
+    params.classList.toggle('open');
+    let description = params.querySelector('.list-unstyled');
+    if(params.classList.contains('open')){
+      description.style.height = `${description.scrollHeight}px`;
+    } else{
+      description.style.height = '0';
+    }
+    removeOpen(ind);
+  })
 }
 
-
-// var accordion = document.querySelectorAll('.accordion');
-// if(accordion){
-//   accordionDrawer()
-// }
-// function accordionDrawer(){
-//   accordion.forEach((item, index)=>{
-//     addOpen(item,index)
-//   })
-// }
-// function addOpen(params, ind){
-//   var header = params.querySelector('.accordion-header');
-//   header.addEventListener('click',()=>{
-//     params.classList.toggle('open');
-//     let description = params.querySelector('.accordion-description');
-//     if(params.classList.contains('open')){
-//       params.querySelector('.down').classList.add('turn-off');
-//       params.querySelector('.up').classList.remove('turn-off');
-//       description.style.height = `${description.scrollHeight}px`;
-//     } else{
-//       description.style.height = '0';
-//       params.querySelector('.down').classList.remove('turn-off');
-//       params.querySelector('.up').classList.add('turn-off');
-//     }
-//     removeOpen(ind);
-//   })
-// }
 
 // function removeOpen(i){
 //   accordion.forEach((item, ind)=>{
