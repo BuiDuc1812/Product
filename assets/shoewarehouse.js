e2a413d9e4aa9da719aea206fdb6fd916255f964
BuiDuc1812 <db939397@gmail.com>
2023-03-20 09:34:47 +0700

    addclass

--- assets/shoewarehouse.js
+++ assets/shoewarehouse.js
@@ -1,31 +1,23 @@
-class ProductJs extends HTMLElement {
-    constructor() {
-        super();
-        console.log(this);
-    }
-    productTabs(){
-        let productTabs = document.querySelector('.product-tabs');
-        if (productTabs) {
-            let titleTabs = productTabs.querySelectorAll('.tabs-title');
-            let contentTabs = productTabs.querySelectorAll('.data-tabs');
-            titleTabs.forEach(item=>{
-                let attribute = item.getAttribute("data-title-name");
-                let content = productTabs.querySelector('[data-block-name='.concat(attribute,']'));
-                item.addEventListener('click',function(){
-                    titleTabs.forEach(item=>{
-                        item.classList.remove('active');
-                    })
-                    contentTabs.forEach(item=>{
-                        item.classList.add('hidden');
-                    })
-                    item.classList.add('active');
-                    content.classList.remove('hidden');
+
+let productTabs = document.querySelector('.product-tabs');
+    if (productTabs) {
+        let titleTabs = productTabs.querySelectorAll('.tabs-title');
+        let contentTabs = productTabs.querySelectorAll('.data-tabs');
+        titleTabs.forEach(item=>{
+            let attribute = item.getAttribute("data-title-name");
+            let content = productTabs.querySelector('[data-block-name='.concat(attribute,']'));
+            item.addEventListener('click',function(){
+                titleTabs.forEach(item=>{
+                    item.classList.remove('active');
                 })
+                contentTabs.forEach(item=>{
+                    item.classList.add('hidden');
+                })
+                item.classList.add('active');
+                content.classList.remove('hidden');
             })
-        }
+        })
     }
-}
-customElements.define('product', ProductJs);
 
 var init = false;
 var swiper = '';
