a70009c0500084002e45a219a480d2f4a065b5c9
BuiDuc1812 <db939397@gmail.com>
2023-03-20 22:14:41 +0700

    change

--- assets/accordion.js
+++ assets/accordion.js
@@ -1,27 +1,24 @@
 let arrcodion = document.querySelectorAll('.footer-block--menu');
-if(arrcodion){
-    window.addEventListener('resize', accordionDrawer)
-}
+
 function accordionDrawer(){
     arrcodion.forEach((item, index)=>{
-        if (window.innerWidth < 990) {
-            addOpen(item,index);
-        }
+      addOpen(item,index);
     })
-
 }
 
 function addOpen(params, ind){
   var header = params.querySelector('.footer-block__heading');
   header.addEventListener('click',()=>{
-    params.classList.toggle('open');
-    let description = params.querySelector('.list-unstyled');
-    if(params.classList.contains('open')){
-      description.style.height = `${description.scrollHeight}px`;
-    } else{
-      description.style.height = '0';
+    if (window.innerWidth < 990) {
+      params.classList.toggle('open');
+      let description = params.querySelector('.list-unstyled');
+      if(params.classList.contains('open')){
+        description.style.height = `${description.scrollHeight}px`;
+      } else{
+        description.style.height = '0';
+      }
+      removeOpen(ind);
     }
-    removeOpen(ind);
   })
 }
 
@@ -35,3 +32,5 @@ function removeOpen(i){
     }
   })
 }
+
+window.addEventListener('resize', addOpen)
