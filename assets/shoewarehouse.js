e693fc653d2977f5af0f7d232ab008e4a4aed5de
BuiDuc1812 <db939397@gmail.com>
2023-03-22 16:23:10 +0700

    format

--- assets/shoewarehouse.js
+++ assets/shoewarehouse.js
@@ -1,22 +1,22 @@
 let productTabs = document.querySelector('.product-tabs');
-    if (productTabs) {
-        let titleTabs = productTabs.querySelectorAll('.tabs-title');
-        let contentTabs = productTabs.querySelectorAll('.data-tabs');
-        titleTabs.forEach(item=>{
-            let attribute = item.getAttribute("data-title-name");
-            let content = productTabs.querySelector('[data-block-name='.concat(attribute,']'));
-            item.addEventListener('click',function(){
-                titleTabs.forEach(item=>{
-                    item.classList.remove('active');
-                })
-                contentTabs.forEach(item=>{
-                    item.classList.add('hidden');
-                })
-                item.classList.add('active');
-                content.classList.remove('hidden');
+if (productTabs) {
+    let titleTabs = productTabs.querySelectorAll('.tabs-title');
+    let contentTabs = productTabs.querySelectorAll('.data-tabs');
+    titleTabs.forEach(item=>{
+        let attribute = item.getAttribute("data-title-name");
+        let content = productTabs.querySelector('[data-block-name='.concat(attribute,']'));
+        item.addEventListener('click',function(){
+            titleTabs.forEach(item=>{
+                item.classList.remove('active');
             })
+            contentTabs.forEach(item=>{
+                item.classList.add('hidden');
+            })
+            item.classList.add('active');
+            content.classList.remove('hidden');
         })
-    }
+    })
+}
 
 var init = false;
 var swiper = '';
