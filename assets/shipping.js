90b7373e82e23e6b85d57ce55bb3568d011382c0
BuiDuc1812 <db939397@gmail.com>
2023-03-24 15:58:24 +0700

    change

---
+++ assets/shipping.js
@@ -0,0 +1,5 @@
+let line = document.getElementById('line');
+let css_line = window.getComputedStyle(line , '::after');
+console.log(css_line)
+// css_line.style.width = '20%'
+// let shipping  = line.get
