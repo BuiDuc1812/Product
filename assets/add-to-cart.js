f155c688b6111261333772948fb6266e7b4dab6f
BuiDuc1812 <db939397@gmail.com>
2023-03-26 22:18:26 +0700

    add to cart

--- assets/add-to-cart.js
+++ assets/add-to-cart.js
@@ -1,10 +1,15 @@
 function quickView(){
     var quickForm = document.querySelectorAll('.form-quick_view');
+    var cartDrawer = document.querySelector('cart-drawer');
+
     if (quickForm) {
         quickForm.forEach(item=>{
-            // console.log(item.getAttribute('product_id'))
+            var btnSubmit = item.querySelector('.ProductSubmitButton');
+            if (cartDrawer) btnSubmit.setAttribute('aria-haspopup', 'dialog');
             item.addEventListener('submit',(e)=>{
                 e.preventDefault();
+                if(btnSubmit.getAttribute('aria-disabled') === 'true') return;
+                btnSubmit.setAttribute('aria-disabled', true);
                 fetch(window.Shopify.routes.root + 'cart/add.js',{
                     method: 'POST',
                     headers: {
@@ -19,6 +24,9 @@ function quickView(){
                 })
                 .then(response => {
                     return response.json();
+                })
+                .then(response => {
+                    window.location = window.routes.cart_url;
                 })
                 .catch((error) => {
                     console.error('Error:', error);
