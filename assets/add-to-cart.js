43dafeef941cbeffcb152e9b2aecf9657cf0bd59
BuiDuc1812 <db939397@gmail.com>
2023-03-27 17:14:53 +0700

    add new code add to cart

--- assets/add-to-cart.js
+++ assets/add-to-cart.js
@@ -1,32 +1,40 @@
+var cartDrawer = document.querySelector('cart-drawer');
+
 function quickView(){
     var quickForm = document.querySelectorAll('.form-quick_view');
-    var cartDrawer = document.querySelector('cart-drawer');
 
     if (quickForm) {
         quickForm.forEach(item=>{
-            console.log(item)
             var btnSubmit = item.querySelector('.ProductSubmitButton');
-            console.log(item)
-
-            if (cartDrawer) btnSubmit.setAttribute('aria-haspopup', 'dialog');
+            btnSubmit.setAttribute('aria-haspopup', 'dialog');
             item.addEventListener('submit',(e)=>{
                 e.preventDefault();
-                if(btnSubmit.getAttribute('aria-disabled') === 'true') return;
-                btnSubmit.setAttribute('aria-disabled', true);
-                fetch(window.Shopify.routes.root + 'cart/add.js',{
+                const product = {
+                    'items':[{
+                        'id': item.getAttribute('product_id'),
+                        'quantity': 1
+                    }]
+                }
+                const config = {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
                     },
-                    body: JSON.stringify({
-                        'items': [{
-                          'id': item.getAttribute('product_id'),
-                          'quantity': 1
-                        }]
-                    })
-                })
+                    body: JSON.stringify(product)
+                }
+                // const formData = new FormData(item);
+                // if (cartDrawer) {
+                //     formData.append('sections', cartDrawer.getSectionsToRender().map((section) => section.id));
+                //     formData.append('sections_url', window.location.pathname);
+                //     cartDrawer.setActiveElement(document.activeElement);
+                // }    
+                fetch(window.Shopify.routes.root + 'cart/add.js', config)
                 .then(response => {
-                    return response.json();
+                    return response.json();                  
+                })
+                .then(dataConfig => {
+                    console.log(dataConfig); 
+                    cartDrawer.classList.add('active')
                 })
                 .catch((error) => {
                     console.error('Error:', error);
@@ -36,3 +44,7 @@ function quickView(){
     }
 }
 quickView()
+
+function loadDataToCart(){
+
+}
