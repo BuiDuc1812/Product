d1363ba03d7862738a3c2e6760ff84588605425c
BuiDuc1812 <db939397@gmail.com>
2023-03-31 15:02:07 +0700

    update css

--- assets/swiper.js
+++ assets/swiper.js
@@ -3,15 +3,20 @@ var swiper = new Swiper(".swiper", {
     breakpoints: {
         0: {
             slidesPerView: 1,
+            spaceBetween: 8
         },
         376: {
           slidesPerView: 2,
+          spaceBetween: 8
+
         },
         750: {
             slidesPerView: 3,
+            spaceBetween: 16
         },
         990: {
             slidesPerView: 4,
+            spaceBetween: 16
         }
     },
     pagination: {
