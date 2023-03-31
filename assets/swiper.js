4c936e5d0d722f0c9a163d5df4b6f711b3ae280a
BuiDuc1812 <db939397@gmail.com>
2023-03-31 10:10:27 +0700

    add collection

--- assets/swiper.js
+++ assets/swiper.js
@@ -1,5 +1,17 @@
 var swiper = new Swiper(".swiper", {
     slidesPerView: 4,
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 2,
+        },
+        750: {
+            slidesPerView: 3,
+        },
+        990: {
+            slidesPerView: 4,
+        }
+    },
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
