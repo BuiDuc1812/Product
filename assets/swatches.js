aac0e851de9e4e6d9cd959a5b744274f197c9014
BuiDuc1812 <db939397@gmail.com>
2023-04-04 17:44:57 +0700

    change

--- assets/swatches.js
+++ assets/swatches.js
@@ -1,5 +1,27 @@
-let form = document.querySelectorAll('.product-swatch');
-form.forEach(item=>{
-    let label = item.querySelector('.label-handle');
-    console.log(label)
-})
+function swatch(){
+    let colorSwatch = document.querySelectorAll('.color-swatch');
+    colorSwatch.forEach(item=>{
+        let colorVariant = item.querySelectorAll('.color-variant');
+        getHandle(colorVariant);
+    })
+}
+
+function getHandle(btn){
+    btn.forEach((item, index)=>{
+        item.addEventListener('click',()=>{
+            item.classList.toggle('choose');
+            removeChoose(btn, index);
+            let handleLink = item.getAttribute('handle');
+        })  
+    })
+}
+
+function removeChoose(btn, ind){
+    btn.forEach((item, i)=>{
+        if(i != ind){
+            item.classList.remove('choose');
+        }
+    })
+}
+
+swatch();
