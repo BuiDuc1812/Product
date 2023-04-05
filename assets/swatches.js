66c04d7ecaae5887d699447a2288e3fae38342fd
BuiDuc1812 <db939397@gmail.com>
2023-04-05 17:49:18 +0700

    change

--- assets/swatches.js
+++ assets/swatches.js
@@ -29,47 +29,40 @@ class swatches {
     }
 
     getData(product, node){
-        fetch('/products/'+ product +'?view=ajax',{
-            headers : { 
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            }
-        })
+        fetch(`/products/${product}?view=ajax`)
         .then(res => {
             return res.json();
         }) 
         .then(data => {
-            this.changeData(data, node)
+            this.changeData(data, node);
         }) 
     }
 
     changeData(data, parent){
         this.liProduct = parent.parentNode.parentNode;
-        this.liProduct.querySelector('.change-img').srcset = data.product.image.src;
-        this.liProduct.querySelector('.handlelink').innerHTML = data.product.title;
-        this.firstVariant = data.product.variants[0]
-        this.liProduct.querySelector('.change-quick').setAttribute('value',this.firstVariant.id);
-        // if (this.firstVariant.available) {
-        //     this.liProduct.querySelector('.soul-tag').style.display = 'none';
+        this.liProduct.querySelector('.change-img').srcset = data.image;
+        this.liProduct.querySelector('.handlelink').innerHTML = data.title;
+        this.liProduct.querySelector('.change-quick').setAttribute('value',data.variant[0].id);
+        if (data.available) {
+            this.liProduct.querySelector('.soul').classList.add('hidden');
+            this.liProduct.querySelector('.quick').classList.remove('hidden');
+            this.liProduct.querySelector('.quick').setAttribute('idvariant',data.id);
+            this.liProduct.querySelector('.quick').setAttribute('quantity',data.variant[0].inventory_quantity);
+            // console.log(this.firstVariant.inventory_quantity)
+        } else {
+            // console.log('het')
+            this.liProduct.querySelector('.soul').classList.remove('hidden');
+            this.liProduct.querySelector('.quick').classList.add('hidden');
+        }
 
-        //     // this.liProduct.querySelector('.soul').classList.add('hidden');
-        //     // this.liProduct.querySelector('.quick').classList.remove('hidden');
-        //     // this.liProduct.querySelector('.quick').setAttribute('idvariant',this.firstVariant.id),
-        //     // this.liProduct.querySelector('.quick').setAttribute('quantity',this.firstVariant.inventory_quantity)
-        //     // console.log(this.firstVariant.inventory_quantity)
-        // } else {
-        //     // this.liProduct.querySelector('.soul').classList.remove('hidden');
-        //     // this.liProduct.querySelector('.quick').classList.add('hidden');
-        // }
-
-        if(this.firstVariant.compare_at_price){
+        if(data.compare_at_price){
             this.liProduct.querySelector('.price__regular').style.display = 'none';
             this.liProduct.querySelector('.price__sale').style.display = 'flex';
             this.liProduct.querySelector('.price__sale').style.flexDirection = "column";
-            this.liProduct.querySelector('.change-pricesale').innerHTML = `$${this.firstVariant.price}`;
-            this.liProduct.querySelector('.change-pricesale-compare').innerHTML = `$${this.firstVariant.compare_at_price}`;
+            this.liProduct.querySelector('.change-pricesale').innerHTML = data.price;
+            this.liProduct.querySelector('.change-pricesale-compare').innerHTML = data.compare_at_price;
         } else {
-            this.liProduct.querySelector('.change-price-regular').innerHTML = `$${this.firstVariant.price}`;
+            this.liProduct.querySelector('.change-price-regular').innerHTML = data.price;
             this.liProduct.querySelector('.price__regular').style.display = 'block';
             this.liProduct.querySelector('.price__sale').style.display = 'none';
         }
