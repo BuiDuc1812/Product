461d7db8dc82b3c9c0f85eaaa5d8cf819d05e5c8
BuiDuc1812 <db939397@gmail.com>
2023-04-10 12:16:58 +0700

    update change variant

--- assets/add-to-cart.js
+++ assets/add-to-cart.js
@@ -1,56 +1,61 @@
-var cartDrawer = document.querySelector('cart-drawer');
+var cartDrawer = document.querySelector("cart-drawer");
 
-function quickView(){
-    var quickForm = document.querySelectorAll('.form-quick_view');
+function quickView() {
+  var quickForm = document.querySelectorAll(".form-quick_view");
 
-    if (quickForm) {
-        quickForm.forEach(item=>{
-            var btnSubmit = item.querySelector('.ProductSubmitButton');
-            btnSubmit.setAttribute('aria-haspopup', 'dialog');
-            item.addEventListener('submit',(e)=>{
-                e.preventDefault();
-                // checkQuanty(btnSubmit).then(()=>{
-                //     ajax(item);
-                // });
-                cancelBtn(btnSubmit, item)
-            })
-        })
-    }
+  if (quickForm) {
+    quickForm.forEach((item) => {
+      var btnSubmit = item.querySelector(".ProductSubmitButton");
+      btnSubmit.setAttribute("aria-haspopup", "dialog");
+      item.addEventListener("submit", (e) => {
+        e.preventDefault();
+        // checkQuanty(btnSubmit).then(()=>{
+        //     ajax(item);
+        // });
+        cancelBtn(btnSubmit, item);
+      });
+    });
+  }
 }
 
-quickView()
+quickView();
 
-function cancelBtn(btn, item){
-    var quantity = btn.getAttribute('quantity');
-    var variantId = btn.getAttribute('idvariant');
-    var quantityCart = cartDrawer.querySelector('input[data-quantity-variant-id="'+`${variantId}`+'"]');
+function cancelBtn(btn, item) {
+  var quantity = btn.getAttribute("quantity");
+  var variantId = btn.getAttribute("idvariant");
+  var quantityCart = cartDrawer.querySelector(
+    'input[data-quantity-variant-id="' + `${variantId}` + '"]'
+  );
 
-    if(quantityCart){
-        if(parseInt(quantityCart.value) < parseInt(quantity)) {
-            ajax(item)
-        } 
-    } else {
-        ajax(item)
+  if (quantityCart) {
+    if (parseInt(quantityCart.value) < parseInt(quantity)) {
+      ajax(item);
     }
+  } else {
+    ajax(item);
+  }
 }
 
-function ajax(form){
-    const formData = new FormData(form);
-    formData.append('sections', cartDrawer.getSectionsToRender().map((section) => section.id));
-    formData.append('sections_url', window.location.pathname);   
- 
-    fetch(window.Shopify.routes.root + 'cart/add.js', {
-        method: 'POST',
-        body: formData
+function ajax(form) {
+  const formData = new FormData(form);
+  formData.append(
+    "sections",
+    cartDrawer.getSectionsToRender().map((section) => section.id)
+  );
+  formData.append("sections_url", window.location.pathname);
+
+  fetch(window.Shopify.routes.root + "cart/add.js", {
+    method: "POST",
+    body: formData,
+  })
+    .then((response) => {
+      return response.json();
     })
-    .then(response => {
-        return response.json();
-    })
-    .then(response => {
-        cartDrawer.renderContents(response);
-        cartDrawer.classList.add('active'); 
+    .then((response) => {
+      cartDrawer.renderContents(response);
+      cartDrawer.classList.add("active");
     })
     .catch((error) => {
-        console.error('Error:', error);
+      console.error("Error:", error);
     });
 }
--- assets/quick-add.js
+++ assets/quick-add.js
@@ -11,17 +11,76 @@ if (!customElements.get('quick-add-modal')) {
 
     show(opener) {
 
-      fetch(`${opener.getAttribute('data-product-url')}?view=ajax`,{
-        headers : { 
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-        }
-      })
-        .then((response) => response.json())
-        .then((response) => {
-          const productItem = response.product;
+      // fetch(`${opener.getAttribute('data-product-url')}?view=ajax`,{
+      //   headers : { 
+      //     'Content-Type': 'application/json',
+      //     'Accept': 'application/json'
+      //   }
+      // })
+      //   .then((response) => response.json())
+      //   .then((response) => {
+      //     const productItem = response.product;
           super.show(opener);
-        })
+          // this.querySelector('.change-variant').addEventListener('change', this.onChangeVariant(productItem));
+        // })
     }
+
+    // onChangeVariant(product) {
+    //   // this.updateOption();
+    //   this.updateIdVariant(product);
+    //   this.updateVariant();
+    // }
+
+    // updateOption() {
+    //   const fieldsets = Array.from(this.querySelectorAll('fieldset'));
+    //   this.options = fieldsets.forEach((fieldset) => {
+    //     const arrVariant = Array.from(fieldset.querySelectorAll('input'));
+    //     // console.log(arrVariant)
+    //     // arrVariant.map(item=>console.log(item));
+    //     // return arrVariant.find(checkEd(arrVariant)).value;
+    //   });
+    // }
+
+    // // checkEd(listVariant){
+    // //   listVariant.map(item => console.log*item);
+    // // }
+    // updateIdVariant(product) {
+    //   this.currentVariant = this.getVariantData(product).find((variant) => {
+    //     // return !variant.options.map((option, index) => {
+    //     //   return this.options[index] === option;
+    //     // }).includes(false);
+    //   });
+    // }
+
+    // getVariantData(product){
+    //   this.variantData = product.variants;
+    //   return this.variantData;
+    // }
+
+    // updateVariant(){
+    //   const selectedOptionOneVariants = this.variantData.filter(variant => this.querySelector(':checked').value === variant.option1);
+    //   console.log(selectedOptionOneVariants);
+      
+    //   const inputWrappers = [...this.querySelectorAll('.product-form__input')];
+    //   inputWrappers.forEach((option, index) => {
+    //     if (index === 0) return;
+    //     const optionInputs = [...option.querySelectorAll('input[type="radio"], option')]
+
+
+    //     const previousOptionSelected = inputWrappers[index - 1].querySelector(':checked').value;
+    //     const availableOptionInputsValue = selectedOptionOneVariants.filter(variant => variant.available && variant[`option${ index }`] === previousOptionSelected).map(variantOption => variantOption[`option${ index + 1 }`]);
+    //     this.setInputAvailability(optionInputs, availableOptionInputsValue)
+    //   });
+    // }
+    
+    // setInputAvailability(listOfOptions, listOfAvailableOptions) {
+    //   listOfOptions.forEach(input => {
+    //     if (listOfAvailableOptions.includes(input.getAttribute('value'))) {
+    //       input.innerText = input.getAttribute('value');
+    //     } else {
+    //       input.innerText = window.variantStrings.unavailable_with_option.replace('[value]', input.getAttribute('value'));
+    //     }
+    //   });
+    // }
   });
 }
