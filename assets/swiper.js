28b43c5d90a5869c2c681ce6aba2ccf73df0c328
BuiDuc1812 <db939397@gmail.com>
2023-04-13 16:56:24 +0700

    change

--- assets/swiper.js
+++ assets/swiper.js
@@ -1,26 +1,50 @@
 var swiper = new Swiper(".swiper-list", {
-    slidesPerView: 4,
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-            spaceBetween: 8
-        },
-        376: {
-          slidesPerView: 2,
-          spaceBetween: 8
-
-        },
-        750: {
-            slidesPerView: 3,
-            spaceBetween: 16
-        },
-        990: {
-            slidesPerView: 4,
-            spaceBetween: 16
-        }
+  slidesPerView: 4,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 8,
     },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    }
+    376: {
+      slidesPerView: 2,
+      spaceBetween: 8,
+    },
+    750: {
+      slidesPerView: 3,
+      spaceBetween: 16,
+    },
+    990: {
+      slidesPerView: 4,
+      spaceBetween: 16,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+function equalHeight() {
+  let talet = 0;
+  const listEqual = document.querySelector(".list-equal");
+  let handle = listEqual.querySelectorAll(".card__content");
+  setHeight(handle, talet);
+  window.addEventListener("resize", function () {
+    setHeight(handle, talet);
   });
+}
+equalHeight();
+
+function setHeight(handle, talet) {
+  handle.forEach((item) => {
+    let titleHeight = item.querySelector(".handlelink");
+    contentBlock(item, talet);
+    contentBlock(titleHeight, talet);
+  });
+}
+
+function contentBlock(item, talet) {
+  let eleHeight = item.offsetHeight;
+  talet = eleHeight > talet ? eleHeight : talet;
+  item.style.height = talet + "px";
+}
